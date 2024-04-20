8bad9fc4947c3954dbc4612b0b45a204ab667b08
Gabriel Oliveira Pinto <c.gabriel.oliveira.p@gmail.com>
2024-04-20 15:44:18 +0100

    feat: add NavigationBar

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,62 @@
+<template>
+  <nav class="navigation-bar">
+    <div class="navigation-bar__logo-container">
+      <img
+        class="navigation-bar__logo"
+        :src="logo"
+      >
+    </div>
+    <div class="navigation-bar__actions">
+      <div class="navigation-bar__thumbnail-container">
+        <img
+          class="navigation-bar__thumbnail"
+          src="@/assets/images/avatar.jpeg"
+        >
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import logo from '@/assets/images/logo.svg';
+</script>
+
+<style lang="scss" scoped>
+.navigation-bar {
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem;
+  background-color: transparent;
+
+  &__actions {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 1rem;
+  }
+
+  &__thumbnail-container {
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+    overflow: hidden;
+  }
+
+  &__thumbnail {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  // .navigation-bar__logo
+  &__logo-container {
+    width: 100px;
+    height: 50px;
+  }
+
+  &__logo {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
