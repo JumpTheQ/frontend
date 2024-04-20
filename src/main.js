a18022153078f766c24cab0c97c052c9753d5ac3
Gabriel Oliveira Pinto <c.gabriel.oliveira.p@gmail.com>
2024-04-20 15:45:12 +0100

    chore: import primevue

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
-import './assets/scss/index.scss'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import 'primevue/resources/themes/aura-light-green/theme.css'
+
 import App from './App.vue'
 import router from './router'
+
+import PrimeVue from 'primevue/config'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue)
 
 app.mount('#app')
