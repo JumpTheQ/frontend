a7e5a4ed38d24a5df9353f9998f0ca96d5768d7f
Gabriel Oliveira Pinto <c.gabriel.oliveira.p@gmail.com>
2024-04-21 01:23:49 +0100

    feat: create DashboardView

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,13 @@
+<template>
+  <DashboardLayout class="dashboard-view">
+    <RouterView />
+  </DashboardLayout>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+
+import DashboardLayout from '@/layouts/DashboardLayout.vue';
+</script>
+
+<style lang="scss" scoped></style>
